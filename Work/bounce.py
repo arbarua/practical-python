4bcebe79f8239832b05212222f080437cba7e69b
Arnab Barua <abarua@mun.ca>
2021-05-23 20:12:43 +0600

    Exercise 1.5 using round()

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+num_of_bounce = 0
+while(num_of_bounce < 10):
+    height *= (3/5)
+    print(num_of_bounce,' ',round(height,4))
+    num_of_bounce+=1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,22 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+num_expay = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    if num_expay < 12:
+        principal = principal * (1+rate/12) - payment - 1000
+        total_paid = total_paid + payment + 1000
+        num_expay+=1
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+print('Total paid', total_paid)
