af28e3f7af9e9b859e97a436989e7a86749daea7
Arnab Barua <abarua@mun.ca>
2021-05-23 20:22:33 +0600

    Execise 1.8 Extra Payment

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,17 +6,17 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 num_expay = 0
-extra_payment_start_month = 61
-extra_payment_end_month = 108
 extra_payment = 1000
+num_of_months = 0
 
 while principal > 0:
+    num_of_months +=1
     if num_expay < 12:
-        principal = principal * (1+rate/12) - payment - 1000
-        total_paid = total_paid + payment + 1000
+        principal = principal * (1+rate/12) - payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
         num_expay+=1
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
 
-print('Total paid', total_paid)
+print('Total paid', total_paid,', Number of months', num_of_months)
