a1caaa95ab28c1eb2e5377ac95ae27f8e9b56c24
Arnab Barua <abarua@mun.ca>
2021-05-23 20:48:45 +0600

    Exercise 1.9 Making Extra Payment

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,12 +6,15 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 num_expay = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 extra_payment = 1000
 num_of_months = 0
 
 while principal > 0:
     num_of_months +=1
-    if num_expay < 12:
+
+    if num_of_months >= extra_payment_start_month and num_of_months <= extra_payment_end_month:
         principal = principal * (1+rate/12) - payment - extra_payment
         total_paid = total_paid + payment + extra_payment
         num_expay+=1
