de45ab41d03c7dbc147baaffcce481559cdbcd31
Arnab Barua <abarua@mun.ca>
2021-05-25 14:16:36 +0600

    Exercise 2.4 A list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+import sys
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+         headers = next(f).split(',')
+         for line in f:
+             row = line.split(',')
+             holding = (row[0], int(row[1]), float(row[2]))
+             portfolio.append(holding)
+    return portfolio
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
