1b85fa8a4c0cf3b0d814bc243a4022fb07f48063
Arnab Barua <abarua@mun.ca>
2021-05-25 16:24:03 +0600

    Exercise 2.7 Gain-Loss

--- Work/report.py
+++ Work/report.py
@@ -1,7 +1,8 @@
 # report.py
 #
-# Exercise 2.4
-import sys
+# Exercise 2.7
+import csv
+
 def read_portfolio(filename):
     portfolio = []
     with open(filename, 'rt') as f:
@@ -12,7 +13,33 @@ def read_portfolio(filename):
              portfolio.append(holding)
     return portfolio
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
+def read_prices(filename):
+    prices = {}
+    f = open('Data/prices.csv', 'r')
+    rows = csv.reader(f)
+    for row in rows:
+         if row != []:
+             prices[row[0]] = float(row[1])
+    return prices
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+total_portfolio_cost = 0.0
+for s in portfolio:
+    total_portfolio_cost += s['shares']*s['price']
+
+print("Total cost:", total_portfolio_cost)
+
+total_prices_cost = 0.0
+for s in portfolio:
+    total_prices_cost += s['shares']*prices[s['name']]
+
+print("Current value:", total_prices_cost)
+
+if total_prices_cost > total_portfolio_cost:
+    print(f"Gain: {total_prices_cost - total_portfolio_cost:.2f}")
+elif total_prices_cost < total_portfolio_cost:
+    print(f"Loss: {total_portfolio_cost - total_prices_cost:.2f}")
 else:
-    filename = 'Data/portfolio.csv'
+    print("No Gain, No Loss")
