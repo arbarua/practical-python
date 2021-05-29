f8da4c343230d14dd0c579a3c7a3d75a84c95c06
Arnab Barua <abarua@mun.ca>
2021-05-30 00:16:48 +0600

    Exercise 3.6: Working without Headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,31 +4,36 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers = True):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
-    
-    # Read the file headers
-        headers = next(rows)
+        
+        if has_headers:
+        # Read the file headers
+             headers = next(rows)
 
-    # If a column selector was given, find indices of the specified columns.
-    # Also narrow the set of headers used for resulting dictionaries
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+        # If a column selector was given, find indices of the specified columns.
+        # Also narrow the set of headers used for resulting dictionaries
+             if select:
+                 indices = [headers.index(colname) for colname in select]
+                 headers = select
+             else:
+                 indices = []
 
         records = []
         for row in rows:
             if not row:    # Skip rows with no data
                 continue
             if types:
-                row = [func(val) for func, val in zip(types, row) ]
-            record = dict(zip(headers, row))
-            records.append(record)
+                if has_headers:
+                    row = [func(val) for func, val in zip(types, row) ]
+                    record = dict(zip(headers, row))
+                    records.append(record)
+                else:
+                    row = [func(val) for func, val in zip(types, row)]
+                    records.append(tuple(row))
 
     return records
