c1252a2bcf85d120192b4a6b43376bc16d66d2bb
Arnab Barua <abarua@mun.ca>
2021-05-29 20:38:47 +0600

    Exercise 3.1 where all major operations, including calculations and output, are carried out by a collection of functions for report.py.

--- Work/report.py
+++ Work/report.py
@@ -3,7 +3,41 @@
 # Exercise 2.7 - 2.12
 import csv
 
+def print_report(report_name):
+    '''prints share names, their prices and change in their prices'''
+    #Header added while completing exercise 2.9 and 2.10 so no need to add separately for exercise 2.11
+    print(f'{"Name":>10} {"Shares":>10} {"price":>10} {"Change":>10}')#make this comment to produce exercise 2.9
+    print("_"*10, "_"*10, "_"*10, "_"*10)#make this comment to produce exercise 2.9
+    for r in report_name:
+    #print(r) "remove the comment marker from this line to produce exercise 2.9 and remove this statment within double quotation"
+        with_dollar = "$"+str(round(r[2],2))# dollar sign added with r[2] = share price
+        print(f'{r[0]:>10s} {r[1]:>10d} {with_dollar:>10} {r[3]:>10.2f}')#make this comment to produce exercise 2.9
+
+def calc_portfolio_cost(portfolio_list):
+    '''calculates total cost of share in portfolio'''
+    calc_total_portfolio_cost = 0.0
+    calc_total_portfolio_cost = sum(int(s['shares'])*float(s['price']) for s in portfolio_list)
+    print("Total cost:", calc_total_portfolio_cost)
+    return calc_total_portfolio_cost
+
+def calc_prices_cost(price_dict, portfolio_list):
+    '''calculate the total prices of the shares present in portfolio'''
+    calc_total_prices_cost = 0.0
+    calc_total_prices_cost = sum(int(s['shares'])*float(price_dict[s['name']]) for s in portfolio_list)
+    print("Current value:", calc_total_prices_cost)
+    return calc_total_prices_cost
+
+def print_gain_loss(prices_cost, portfolio_cost):
+    '''prints if there is gain or loss'''
+    if prices_cost > portfolio_cost:
+        print(f"Gain: {total_prices_cost - total_portfolio_cost:.2f}")
+    elif prices_cost < portfolio_cost:
+        print(f"Loss: {total_portfolio_cost - total_prices_cost:.2f}")
+    else:
+        print("No Gain, No Loss")
+
 def read_portfolio(filename):
+    '''reads porfolio file and make a list with the components in the file'''
     portfolio = []
     with open(filename, 'rt') as f:
          rows = csv.reader(f)
@@ -14,6 +48,7 @@ def read_portfolio(filename):
     return portfolio
 
 def read_prices(filename):
+    '''reads file containing the prices of the shares and put the components in a dictionary'''
     prices = {}
     f = open('Data/prices.csv', 'r')
     rows = csv.reader(f)
@@ -22,42 +57,27 @@ def read_prices(filename):
              prices[row[0]] = float(row[1])
     return prices
 
-portfolio = read_portfolio('Data/portfoliodate.csv')
-prices = read_prices('Data/prices.csv')
-
-total_portfolio_cost = 0.0
-for s in portfolio:
-    total_portfolio_cost += int(s['shares'])*float(s['price'])
-
-print("Total cost:", total_portfolio_cost)
-
-total_prices_cost = 0.0
-for s in portfolio:
-    total_prices_cost += int(s['shares'])*float(prices[s['name']])
-
-print("Current value:", total_prices_cost)
-
-if total_prices_cost > total_portfolio_cost:
-    print(f"Gain: {total_prices_cost - total_portfolio_cost:.2f}")
-elif total_prices_cost < total_portfolio_cost:
-    print(f"Loss: {total_portfolio_cost - total_prices_cost:.2f}")
-else:
-    print("No Gain, No Loss")
-
-
 def make_report(portfolio, prices):
+    '''make a report list with name, share cost, share price and changes'''
     report = []
     for s in portfolio:
         report.append((s['name'], int(s['shares']), float(prices[s['name']]), float(prices[s['name']])-float(s['price'])))
     return report
 
+portfolio = read_portfolio('Data/portfoliodate.csv')
+prices = read_prices('Data/prices.csv')
+total_portfolio_cost = calc_portfolio_cost(portfolio)
+total_prices_cost = calc_prices_cost(prices, portfolio)
+print(total_prices_cost, total_portfolio_cost)
+print_gain_loss(total_prices_cost, total_portfolio_cost)
 report = make_report(portfolio, prices)
+print_report(report)
 
-#Header added while completing exercise 2.9 and 2.10 so no need to add separately for exercise 2.11
+'''#Header added while completing exercise 2.9 and 2.10 so no need to add separately for exercise 2.11
 print(f'{"Name":>10} {"Shares":>10} {"price":>10} {"Change":>10}')#make this comment to produce exercise 2.9
 print("_"*10, "_"*10, "_"*10, "_"*10)#make this comment to produce exercise 2.9
 for r in report:
     #print(r) "remove the comment marker from this line to produce exercise 2.9 and remove this statment within double quotation"
     with_dollar = "$"+str(round(r[2],2))# dollar sign added with r[2] = share price
-    print(f'{r[0]:>10s} {r[1]:>10d} {with_dollar:>10} {r[3]:>10.2f}')#make this comment to produce exercise 2.9
+    print(f'{r[0]:>10s} {r[1]:>10d} {with_dollar:>10} {r[3]:>10.2f}')#make this comment to produce exercise 2.9'''
 
