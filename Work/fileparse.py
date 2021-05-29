0ccec0bdc8fb787762f6cf4a658d6ab9f3114c80
Arnab Barua <abarua@mun.ca>
2021-05-30 00:23:25 +0600

    Exercise 3.7: Picking a different column delimitier

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,13 +4,16 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers = True):
+def parse_csv(filename, select=None, types=None, has_headers = True, delimiter = None):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
-        
+        if delimiter:
+            rows = csv.reader(f, delimiter = delimiter)
+        else:
+            rows = csv.reader(f)
+
         if has_headers:
         # Read the file headers
              headers = next(rows)
