a720058a06a3f69e477301f524316971462f7869
Arnab Barua <abarua@mun.ca>
2021-05-30 16:10:26 +0600

    Exercise 3.14: Using more library imports

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,22 +2,20 @@
 #
 # Exercise 1.27
 import sys
-import csv
+#import csv
+#import fileparse
+import report
 def portfolio_cost(filename): 
-    total_cost = 0
-    with open(filename, 'rt') as f:
-         rows = csv.reader(f)
-         headers = next(rows)
-         for line_no,line in enumerate(rows, start = 1):
-             record = dict(zip(headers, line))
-             try:
+    total_cost = 0           
+    for record in report.read_portfolio(filename):
+          try:
                  num_share = int(record['shares'])
                  share_price  = float(record['price'])
-             except ValueError:
+          except ValueError:
                  print(f'row no #{line_no}: Contains missing values: [{line}]: replaced with 0s')
                  num_share = 0
-                 share_price = 0                     
-             total_cost = total_cost+ (num_share * share_price)
+                 share_price = 0
+          total_cost = total_cost+ (num_share * share_price)
     return total_cost
 
 if len(sys.argv) == 2:
